ee2d3c249271b27ec957da81f7c5ceeed901c05a
iitjee <iit.jee.help.me@gmail.com>
2017-02-01 17:12:21 +0530

    Create 7 Default Props and PropTypes.js

---
+++ React/7 Default Props and PropTypes.js
@@ -0,0 +1,34 @@
+/*
+
+
+Default props
+An optional but very useful feature that use in React is default properties.
+When we setup defaults, we can use the default values if another value is not provided.
+
+
+
+
+
+
+*/
+
+
+(SkiDayCount.js in version which we used class-type component)
+
+          getDefaultProps() { //just after export const SkiDayCount = createClass({
+              return {
+                total: 50,
+                powder: 50, 
+                backcountry: 15, 
+                goal: 100
+              }
+            },
+
+
+
+
+
+
+
+
+
