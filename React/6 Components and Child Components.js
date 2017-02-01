962f6621c0945feecc3da1233044eb0673274ea5
iitjee <iit.jee.help.me@gmail.com>
2017-02-01 17:06:00 +0530

    Update 6 Components and Child Components.js

--- React/6 Components and Child Components.js
+++ React/6 Components and Child Components.js
@@ -1,17 +1,20 @@
-
-
+//we've 4 properties in our component
+//     -Total, (string)
+//     - powder, (string)
+//     - back country :Is it a backcountry day or no?(Bool)
+//     - goal: Is it a backcountry day or no?(boolean)
 
 
 /*  Lets make a table called `SkiDayList`   */
 (index.js)
             import React from 'react'
             import { render } from 'react-dom'
-            import { SkiDayList } from './components/SkiDayList'  //changed from SkiDayCount to the new SkiDayList
+            import { SkiDayList } from './components/SkiDayList'  //changed from SkiDayCount to the new SkiDayList which we'll create in a moment
 
             window.React = React
 
             render(
-              <SkiDayList days={  //days is a new prop
+              <SkiDayList days={  //days is a new prop of type array
                 [
                   {
                     resort: "Squaw Valley",
@@ -35,6 +38,69 @@
               }/>,
               document.getElementById('react-container')
             )
+
+
+//create ./components/skidayrow.js and  ./components/skidaylist.js
+(skidayrow.js)
+            import Terrain from 'react-icons/lib/md/terrain'
+            import SnowFlake from 'react-icons/lib/ti/weather-snow'
+            import Calendar from 'react-icons/lib/fa/calendar'
+
+            export const SkiDayRow = ({resort, date, powder, backcountry}) => (
+                        <tr>
+                                    <td>
+                                                {date.getMonth()+1}/{date.getDate()}/{date.getFullYear()} //{date.getMonth() etc are zero-indexed
+                                    </td>
+                                    <td>
+                                                {resort}
+                                    </td>
+                                    <td>
+                                                {(powder) ? <SnowFlake/> : null}    //JSX if-else ternary equivalent            
+                                    </td>
+                                    <td>
+                                                {(backcountry) ? <Terrain /> : null}
+                                    </td>
+                        </tr>						
+
+            )
+             
+                        
+(skidaylist.js)
+                        import Terrain from 'react-icons/lib/md/terrain'
+                        import SnowFlake from 'react-icons/lib/ti/weather-snow'
+                        import Calendar from 'react-icons/lib/fa/calendar'
+                        import { SkiDayRow } from './SkiDayRow'
+
+                        export const SkiDayList = ({days}) => (
+                                    <table>
+                                                <thead>
+                                                     <tr>
+                                                        <th>Date</th>
+                                                        <th>Resort</th>
+                                                        <th>Powder</th>
+                                                        <th>Backcountry</th>
+                                                     </tr>
+                                                </thead>
+                                                <tbody>
+                                                        {days.map((day, i) => <SkiDayRow key={i} 
+                                                                                         resort={day.resort}
+                                                                                         date=day.date()
+                                                                                         powder={day.powder}
+                                                                                         backcountry={day.backcountry}
+                                                                                />	
+                                                                        )}
+                                    //what this does this for every day in the 'days' array, we're going to render a new row
+                                    //spread operator: instead of setting up all of these different properties, if we would just want to push all of the properties from the array into the props objects, 
+                                    // <SkiDayRow key={i} {...day}/>
+                                    // This is just going to take any keys that are part of that object and make them accessible by the SkiDayRow. 
+                                                </tbody>
+
+                                    </table>
+                        )
+
+
+
+
 
 
 
