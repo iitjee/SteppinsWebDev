e8f7416b5ac70046159993488fce4e7ab465bbb8
iitjee <iit.jee.help.me@gmail.com>
2017-02-03 00:40:47 +0530

    Update 8 Prop Types.js

--- React/8 Prop Types.js
+++ React/8 Prop Types.js
@@ -11,19 +11,66 @@ Note:  For performance reasons, propTypes is only checked in development mode.
 
 /*  Type 1: Creating Components using Class */
 (SkiDayCount-createClass.js)
+
+                import { createClass, PropTypes } from 'react'
 //Just after `export const SkiDayCount = createClass({` and before `getDefaultProps() {`
                 propTypes: {
-                    total: PropTypes.number.isRequired,
+                    total: PropTypes.number.isRequired, //make sure a warning is shown if the prop isn't provided. (if defaults are there,this will never give a warning)
                     powder: PropTypes.number,
                     backcountry: PropTypes.number
                   },
+//note that we've destructured PropTypes from 'react'. Hadn't we done that, we should use React.propTypes.number etc
+
+(index.js)
+                import { SkiDayCount } from './components/SkiDayCount-createClass.js'
+
+                render(
+                  <SkiDayCount total="lots" />,   //we're intentionally giving wrong type(string) to `total` (which shuld have number type)
+                  document.getElementById('react-container')
+                )
 
 
+//Once you run, You'll see that it will still render but in console, you will get a warning!
+                
+                
 
+/*  Type-2: Using ES6 Syntax */
+(in SkiDayCount-ES6.js)
+            import { Component, PropTypes } from 'react'
 
+//completely outside the class definition
+              SkiDayCount.propTypes = {
+                total: PropTypes.number,
+                powder: PropTypes.number,
+                backcountry: PropTypes.number
+              }
 
+(in index.js)
+              import { SkiDayCount } from './components/SkiDayCount-ES6.js'
 
+              render( //supplied wrong type(boolean) instead of number
+                <SkiDayCount backcountry={false} />,  
+                document.getElementById('react-container')
+              )
 
+/* Type-3: Statless Function Types  */
+(in SkiDay.js)
+              import { PropTypes } from 'react'
+  
+//completely outside the class definition
+              SkiDayCount.propTypes = {
+                total: PropTypes.number,
+                powder: PropTypes.number,
+                backcountry: PropTypes.number
+              }
+
+(in index.js)
+              import { SkiDayCount } from './components/SkiDayCount'
+
+              render(
+                <SkiDayCount backcountry={false} />, 
+                document.getElementById('react-container')
+              )
 
 
 
