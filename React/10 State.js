4d67b930ebf84ff7eb71d7b7e48dc15668458b6d
iitjee <iit.jee.help.me@gmail.com>
2017-02-03 13:40:05 +0530

    Update 10 State.js

--- React/10 State.js
+++ React/10 State.js
@@ -8,7 +8,7 @@ This way we can avoid overwriting state variables, which can cause chaos in our 
 */
 
 
-
+/*	Type-1: for components created with createClass method	*/
 
 //Create a new component called "App"
 //(make new file "app.js" in Components flder
@@ -101,9 +101,67 @@ countDays(filter) {
       */
 	}
 
-              
-              
-              
+/*	Type 2: Components created with ES6 Class syntax	*/
+// When creating stateful components with ES6, the syntax is very similar to createClass but there are a few things to look out for.
+(In App.js)
+	//add `import { Component } from 'react'
+        //change export const App = createClass(..) to export class App extends Component {..}
+	//remove commas within the methods which shouldn't be there in class syntax
+
+	//There's a different method that we need to add to our class component here and it's called constructor.
+	//So constructor is going to be responsible for taking in props. We're going to call our super class and we're going 
+	//to add some new state data to our class.
+	import { Component } from 'react'
+	import { SkiDayList } from './SkiDayList'
+	import { SkiDayCount } from './SkiDayCount'
+
+	export class App extends Component {
+		constructor(props) {
+			super(props)
+			this.state = {	//instead of using getInitialState, we use this.state property and assign it our array	 object
+				allSkiDays: [
+				{
+					resort: "Squaw Valley",
+					date: new Date("1/2/2016"),
+					powder: true,
+					backcountry: false
+				},
+				{
+					resort: "Kirkwood",
+					date: new Date("3/28/2016"),
+					powder: false,
+					backcountry: false
+				},
+				{
+					resort: "Mt. Tallac",
+					date: new Date("4/2/2016"),
+					powder: false,
+					backcountry: true
+				}
+			]
+			}
+		}
+		countDays(filter) {
+			const { allSkiDays } = this.state
+			return allSkiDays.filter(
+				(day) => (filter) ? day[filter] : day).length
+		}
+		render() {
+			return (
+				<div className="app">
+					<SkiDayList days={this.state.allSkiDays}/>
+					<SkiDayCount total={this.countDays()}
+								 powder={this.countDays(
+										"powder"
+									)}
+								 backcountry={this.countDays(
+										"backcountry"
+									)}/>
+				</div>
+			)
+		}
+	}
+
               
               
               
