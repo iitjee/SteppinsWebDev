537c39c0a1343a7ae4a9683d3fb45db11499ccfb
iitjee <iit.jee.help.me@gmail.com>
2017-02-03 21:51:33 +0530

    Create 13 Route Parameters.js

---
+++ React/13 Route Parameters.js
@@ -0,0 +1,9 @@
+/*
+
+Currently, our ski day list component is rendering all of our different ski days. In this video, we're going 
+to add some filters so that I can see just the powder days and just the back country days, based on links that 
+I click on. Over in our ski day list component, we're going to make a few changes that will make this look much, much better.
+
+
+
+*/
