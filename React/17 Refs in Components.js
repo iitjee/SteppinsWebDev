5916e80de70c066428ffc9c8e5f510da120df0f4
iitjee <iit.jee.help.me@gmail.com>
2017-02-05 00:47:03 +0530

    Update 17 Refs in Components.js

--- React/17 Refs in Components.js
+++ React/17 Refs in Components.js
@@ -14,7 +14,7 @@ of these refs to our input fields
       return (
             <form onSubmit={this.submit} className="add-day-form">
 
-              <label htmlFor="resort">Resort Name</label>
+              <label htmlFor="resort">Resort Name</label> //notice this 'htmlFor'. Since 'for' is a keyword reserved in js, we use htmlFor in JSX
               <input id="resort" 
                    type="text" 
                    required 
@@ -45,6 +45,51 @@ of these refs to our input fields
                   Backcountry Day
                 </label>
               </div>
-              <button>Add Day</button>
+            
+              <button>Add Day</button> //new
+            
             </form>
           )
+
+/* Awesome. So now that we have created refs for all of our inputs, we need to handle the submission of this form, or
+we actually need to capture these values. So let's go ahead to our component, and we're going to add submit as a new 
+function.*/
+
+export class AddDayForm extends Component {
+	
+	constructor(props) {
+		super(props)
+		this.submit = this.submit.bind(this)
+	}
+
+	submit(e) { //new
+		e.preventDefault()
+		console.log('resort', this.refs.resort.value)
+		console.log('date', this.refs.date.value)
+		console.log('powder', this.refs.powder.checked)
+		console.log('backcountry', this.refs.backcountry.checked)
+// So just like we have a props object that we can use this.refs to grab, we can use refs the same way. 
+	}
+
+	render() {
+            ...
+            return (
+			<form onSubmit={this.submit} className="add-day-form">
+                  ...
+      }
+                  
+                  
+// let's set up our constructor first. So we'lL send in props to the constructor and to super. We need to bind this. So when we create a custom method and we need to use this for this.submit or anything else, we need to have this accessible. So all we need to do here is use this.submit.bind(this).
+      export class AddDayForm extends Component {
+	
+	constructor(props) {
+		super(props)
+		this.submit = this.submit.bind(this)
+	}
+
+	submit(e) {...}
+
+	render() {...}
+                  
+                  
+                  
