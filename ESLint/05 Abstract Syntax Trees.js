39ddddd6dfb2113738206fc5c45a1eea13233ed1
iitjee <iit.jee.help.me@gmail.com>
2017-02-06 04:26:19 +0530

    Create 05 Abstract Syntax Trees.js

---
+++ ESLint/05 Abstract Syntax Trees.js
@@ -0,0 +1,23 @@
+/*
+
+AST = A js object representation of our code
+
+Let's see how ESLint analyzes your code by using abstract syntax trees
+WEbpack, babel.js, linters, minifiers, syntax highlighters many uses this
+
+
+
+console.log('hello'); ------> {'type':.. 'expression':.. arguments:..} AST
+
+astexplorer.net
+
+
+
+
+*/
+
+
+
+
+
+
