a4440351e6a594c0376f890aedb526609383b32a
iitjee <iit.jee.help.me@gmail.com>
2017-02-05 22:49:30 +0530

    Create 0 Intro.js

---
+++ React/Project1- React n Redux/0 Intro.js
@@ -0,0 +1,21 @@
+Redux :    
+ES6 with babel 
+React Router
+WebPack
+npm scripts
+ESLint
+Mocha, React Test Utils, and Enzyme
+Webstorm as editor
+
+
+why redux is so popular?
+1. Redux centralizes all of your application state in one store: a single centralized object graph
+ this makes it easier interactions
+2. Reduced boilerplate. Your top-level container(app.js) components are subscribed to the store n a very elegant way.
+3. It's architecture is friendly for server rendering and react components
+which is commonly referred to as isomorphic or universal js.
+4. Has an immutable store => more performance
+5. Hot reloading => instant viewing
+6. Time-travel debugging: allows you to step forward and backward through state changes in your code
+and even replay interactions
+7. Small: 2Kb minified
