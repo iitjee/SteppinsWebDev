0ea460d1e9be44bc90865a81a176a85e16e6e178
iitjee <iit.jee.help.me@gmail.com>
2017-02-05 23:30:43 +0530

    Create 01 Environment Setup.js

---
+++ React/Project1- React n Redux/01 Environment Setup.js
@@ -0,0 +1,34 @@
+
+
+scaffolder:
+https://github.com/coryhouse/react-slingshot
+
+^we won't use this though
+we'll use: https://github.com/coryhouse/pluralsight-redux-starter
+
+Our dev environment:
+1. Automated Testign
+2. Linting
+3. Minification
+4. Bundling
+5. JSXCompilation
+6. ES6 Transpilation
+
+Babel-Polyfill:
+few things babel can't do and babel-polyfill can do for es6->5
+array.from, set, map, promise, generator functions etc.
+Downsides: It's too large 50K but we can pull out whatever particula polyfill we want
+
+babel-preset-react-hmre
+it's a babel preset that wraps up a number of other libraries and settings in a single prest
+It works by wrapping your components in a custom proxy using Babel.
+Proxies are classes that act just like your classes but  they provide hooks for injectin new implementiaton
+So when you hit save, your changes are immd8ly applied without requiring a reload
+WARNINGS: experimental and doesn't reload functional  components and container functions like mapStateTOprops
+(I think this is embedded into react now)
+
+
+npm scripts replaced gulp! cool right!
+https://medium.freecodecamp.com/why-i-left-gulp-and-grunt-for-npm-scripts-3d6853dd22b8#.scjzloi78
+
+  
