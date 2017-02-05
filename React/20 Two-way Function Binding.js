858635bba865df62de7212a6832eb5e652c7334f
iitjee <iit.jee.help.me@gmail.com>
2017-02-05 22:22:16 +0530

    Create 20 Two-way Function Binding.js

---
+++ React/20 Two-way Function Binding.js
@@ -0,0 +1,17 @@
+/*
+
+Now that we have access to the form values we need to pass them up the component tree to the parent, the App. 
+We can do this using two-way function binding. Once we pass the data back up to the App, we're going to change the state. 
+
+
+
+
+*/
+
+
+
+
+
+
+
+
