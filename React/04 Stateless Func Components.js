2fad5de54c345727639cfebda1665be31a24c825
iitjee <iit.jee.help.me@gmail.com>
2017-02-08 16:23:55 +0530

    Update 04 Stateless Func Components.js

--- React/04 Stateless Func Components.js
+++ React/04 Stateless Func Components.js
@@ -10,6 +10,27 @@ Component Syntax:
  Stateless components offer a functional way to work with components, and, also, the React team has hinted that there may
  be some performance benefits of using these functions, rather than using createClass, or ES6 classes. 
 
+    Uses:
+    1. No class needed. Functions!!
+   
+    2. Lets you avoid 'this' keyword. uff!
+    
+    3. Enforced best practices. Useful for dumb presentational components.
+    Presentational components focus on the UI rather than the behavoir.
+    So it's important to avoid using state in presentational components.
+    Instead, state should be managed by higher level container components or via flux, redux etc.
+    
+    4. Doesn't support life-cycle methods and hence protects us from lazyness
+    It's always temptign to add a little state to our presentational component when  
+    you're in a hurry.. It's a quick way to hack in a feature but stateless components
+    don't support local state it's not easy to hack in. So you're forced to put state management wehre it belongs
+    in hihger level container components.
+    
+    5. High signal-to-noise ratio. i.e less typing
+    
+    6. Easier to understand. 
+    Stateless functional components are functions that take in property information, and spits out JSX elements.
+    => Easy to test and assertions are easier
 
 */
 
@@ -23,9 +44,12 @@ Component Syntax:
           //we are no longer using Component from react so we can remove that import statement as well
           import '../stylesheets/ui.scss'
 
+          //we'll use const. In ES6 'var' keyword should be avoided. we should use 'let' or 'const'
+          //const to ensure we don't reassign the component
           const percentToDecimal = (decimal) => { //function declarartion way is also changed. notice
-            return ((decimal * 100) + '%')
-          }
+            return ((decimal * 100) + '%') //here react assumes your return statemntis your render function
+          } //they have no state. Hence the name. They get their data solely from props, which are
+          //are immutable
 
           const calcGoalProgress = (total, goal) => {
             return percentToDecimal(total/goal)
