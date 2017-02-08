8a6aa9c3786e9d370fb497caccb1d1da7bbf6570
iitjee <iit.jee.help.me@gmail.com>
2017-02-08 20:40:52 +0530

    Update and rename Header Compo App.js to 10 Header Compo App.js

--- React/Project1- React n Redux/10 Header Compo App.js
+++ React/Project1- React n Redux/10 Header Compo App.js
@@ -1,7 +1,7 @@
 
 
-Let's make a top-level component which will be used in every webpage like header for eg.
-Let's name this file App.js in components dir
+/*Let's make a top-level component which will be used in every webpage like header for eg.
+Let's name this file App.js in components dir*/
 
 (in components/App.js)
 // This component handles the App template used on every page.
