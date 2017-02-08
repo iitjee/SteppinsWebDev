bc6cda07844f6ea05b26c5b843a470c66211d477
iitjee <iit.jee.help.me@gmail.com>
2017-02-08 20:40:19 +0530

    Create Header Compo App.js

---
+++ React/Project1- React n Redux/Header Compo App.js
@@ -0,0 +1,33 @@
+
+
+Let's make a top-level component which will be used in every webpage like header for eg.
+Let's name this file App.js in components dir
+
+(in components/App.js)
+// This component handles the App template used on every page.
+import React, {PropTypes} from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <p> Header here... </p>
+        {this.props.children} //passing child props
+      </div>
+    );
+  }
+}
+
+App.propTypes = { //type validation
+  children: PropTypes.object.isRequired, //adding children as a required PropType on this component
+};
+
+export default App;
+
+
+
+
+
+
+
