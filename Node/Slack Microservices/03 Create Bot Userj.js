94f43e8f149fc7694d10badb22081afd232b1609
iitjee <iit.jee.help.me@gmail.com>
2017-02-08 23:01:27 +0530

    Create 03 Create Bot Userj.js

---
+++ Node/Slack Microservices/03 Create Bot Userj.js
@@ -0,0 +1,17 @@
+https://allzzwell.slack.com/messages/general/
+
+Let's use a module called node.slack-stk, it's provided by the Slack team.
+ This is a trustworthy source, and we can expect that the module will always be on par with Slack's API. 
+https://github.com/slackapi/node-slack-sdk
+ 
+ 
+https://api.slack.com/bot-users
+ctrlF for ' creating a new bot user.' link and go there
+Give name to bot
+copy access tok (msgd to slackbot)
+click 'save integratoin' @bottom
+
+
+
+
+
