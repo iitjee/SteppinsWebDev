04b49a3cb59cdd0fdff11bfc437e6f40bd3e3df1
iitjee <iit.jee.help.me@gmail.com>
2017-02-21 17:29:17 +0530

    Create 04 Running App.js

---
+++ ReactNative/04 Running App.js
@@ -0,0 +1,20 @@
+Previously we modified AppContainer.js to have a navigator inside menu. Now let's make it the single entry point for application.
+
+(in index.ios.js)
+//modify like this
+
+import React, { Component } from 'react';
+import {
+  AppRegistry, //remoed other modules
+} from 'react-native';
+
+import AppContainer from './app/AppContainer' //imported our AppContainer
+export default class dinder extends Component {
+  render() {
+    return (
+      <AppContainer/>
+    );
+  }
+}
+
+AppRegistry.registerComponent('dinder', () => dinder);
