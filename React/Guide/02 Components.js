18f1080bce836316bf7b5f2576b5aa77134e02a9
iitjee <iit.jee.help.me@gmail.com>
2017-02-22 07:45:36 +0530

    Update 02 Components.js

--- React/Guide/02 Components.js
+++ React/Guide/02 Components.js
@@ -14,7 +14,7 @@ You can also use an ES6 class to define a component:
       }
     }
     
-The above two components are equivalent from React's point of view.
+The above two components are equivalent from Reacts point of view.
 Classes have some additional features that we will discuss in the next sections. Until then, we will use functional components
 for their conciseness
 
@@ -137,4 +137,22 @@ function Comment(props) {
 }
 
 Extracting components might seem like grunt work at first, but having a palette of reusable components pays off in larger apps. A good rule of thumb is that if a part of your UI is used several times (Button, Panel, Avatar), or is complex enough on its own (App, FeedStory, Comment), it is a good candidate to be a reusable component.
+
+Props are Read-Only:
+Whether you declare a component as a function or a class, it must never modify its own props. Consider this sum function:
+        function sum(a, b) {
+          return a + b;
+        }
+Such functions are called "pure" because they do not attempt to change their inputs, and always return the same result for the same inputs.
+In contrast, this function is impure because it changes its own input:
+
+    function withdraw(account, amount) {
+      account.total -= amount;
+    }
+React is pretty flexible but it has a single strict rule:
+All React components must act like pure functions with respect to their props.
+Of course, application UIs are dynamic and change over time. In the next section, we will introduce a new concept of "state".
+State allows React components to change their output over time in response to user actions, network responses, and anything
+else, without violating this rule.
+
 
