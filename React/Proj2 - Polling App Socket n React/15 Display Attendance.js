ce0850f806c2a08c5f080ff6b3b1d22257f3c26d
iitjee <iit.jee.help.me@gmail.com>
2017-02-23 18:00:48 +0530

    Create 15 Display Attendance.js

---
+++ React/Proj2 - Polling App Socket n React/15 Display Attendance.js
@@ -0,0 +1,41 @@
+
+// Once a speaker has connected, they will get to see information about each of the connected audience members in a table. We //will create an attendance component to handle this task.
+
+var React = require('react');
+
+var Attendance = React.createClass({
+
+	addMemberRow(member, i) {
+  //this function is the callback function to the array map function, which means that it's going to be invoked once for every 
+//audience member and that audience member is going to be passed as an argument to this function.
+		return (
+			<tr key={i}>
+				<td>{member.name}</td>
+				<td>{member.id}</td>
+			</tr>
+		);
+	},
+
+	render() {
+		return (
+			<div>
+				<h2>Attendance -- {this.props.audience.length}</h2>
+				<table className="table table-stripped"> //bootstrap classes
+					<thead> //tablehead
+						<tr>
+							<th>Audience Member</th>
+							<th>Socket ID</th>
+						</tr>
+					</thead>
+					<tbody>
+
+						{this.props.audience.map(this.addMemberRow)} //the map function is going to return, for us, an array of table row jsx elements.
+
+					</tbody>
+				</table>
+			</div>
+		);
+	}
+});
+
+module.exports = Attendance;
