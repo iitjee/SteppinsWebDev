3da026a1f1c95e9eae8abcf71e817604ed7073ba
iitjee <iit.jee.help.me@gmail.com>
2017-03-01 18:17:53 +0530

    Update 04 Running App.js

--- ReactNative/04 Running App.js
+++ ReactNative/04 Running App.js
@@ -1,4 +1,5 @@
-Previously we modified AppContainer.js to have a navigator inside menu. Now let's make it the single entry point for application.
+//Previously we modified AppContainer.js to have a navigator inside menu. Now let's make it the single entry point for
+//application.
 
 (in index.ios.js)
 //modify like this
@@ -11,10 +12,33 @@ import {
 import AppContainer from './app/AppContainer' //imported our AppContainer
 export default class dinder extends Component {
   render() {
-    return (
+    return ( //remove all other jsx except appcontainer
       <AppContainer/>
     );
   }
 }
 
 AppRegistry.registerComponent('dinder', () => dinder);
+
+$react-native run-ios
+(from now on  you can do cmd+R to refresh simulator)
+    
+    
+    
+//Similarly in Android
+    import React, { Component } from 'react';
+    import {
+      AppRegistry
+    } from 'react-native';
+    
+    import AppContainer from './app/AppContainer'
+    export default class dinder extends Component {
+      render() {
+        return (
+          <AppContainer/>
+        );
+      }
+    }
+
+
+    AppRegistry.registerComponent('dinder', () => dinder);
