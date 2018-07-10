c0ff59fddbdc22c21a16fd1561ef19f59f0d8d87
iitjee <iit.jee.help.me@gmail.com>
2018-07-10 21:27:23 +0530

    Update 04 addFunctionality.js

--- ChromeExtension/04 addFunctionality.js
+++ ChromeExtension/04 addFunctionality.js
@@ -14,10 +14,44 @@ further user interaction. Update popup.js to include the following code.
             
             
 run it and see! :D
-you won't get! :p because you need to add permissions
+you wont get! :p because you need to add permissions
 (manifest.json)
     "permissions": ["activeTab", "declarativeContent", "storage"],
 
 
 now reload and see xD
+
+
+
+The extension currently only allows users to change the background to green. Including an options page gives users more control over the 
+extension's functionality, further customizing their browsing experience.
+(create options.html)
+                       <!DOCTYPE html>
+                        <html>
+                          <head>
+                            <style>
+                              button {
+                                height: 30px;
+                                width: 30px;
+                                outline: none;
+                                margin: 10px;
+                              }
+                            </style>
+                          </head>
+                          <body>
+                            <div id="buttonDiv">
+                            </div>
+                            <div>
+                              <p>Choose a different background color!</p>
+                            </div>
+                          </body>
+                          <script src="options.js"></script>
+                        </html>
+
+Then register the options page in the manifest,
+    "options_page": "options.html",
+
+               
+go to chrome://extensions near your extension and click Details
+and checkoout "extension options"
 
