cbb47d0ef56e3485e82e093ec9c47659bb0d1e49
iitjee <iit.jee.help.me@gmail.com>
2019-03-23 11:26:33 +0530

    Update 01 Environment Setup.js

--- React/Project1- React n Redux/01 Environment Setup.js
+++ React/Project1- React n Redux/01 Environment Setup.js
@@ -3,8 +3,8 @@
 scaffolder:
 https://github.com/coryhouse/react-slingshot
 
-^we won't use this though
-we'll use: https://github.com/coryhouse/pluralsight-redux-starter
+^we wont use this though
+well use: https://github.com/coryhouse/pluralsight-redux-starter
 
 Our dev environment:
 1. Automated Testign
@@ -15,16 +15,16 @@ Our dev environment:
 6. ES6 Transpilation
 
 Babel-Polyfill:
-few things babel can't do and babel-polyfill can do for es6->5
+few things babel cant do and babel-polyfill can do for es6->5
 array.from, set, map, promise, generator functions etc.
-Downsides: It's too large 50K but we can pull out whatever particula polyfill we want
+Downsides: Its too large 50K but we can pull out whatever particula polyfill we want
 
 babel-preset-react-hmre
-it's a babel preset that wraps up a number of other libraries and settings in a single prest
+its a babel preset that wraps up a number of other libraries and settings in a single prest
 It works by wrapping your components in a custom proxy using Babel.
 Proxies are classes that act just like your classes but  they provide hooks for injectin new implementiaton
 So when you hit save, your changes are immd8ly applied without requiring a reload
-WARNINGS: experimental and doesn't reload functional  components and container functions like mapStateTOprops
+WARNINGS: experimental and doesnt reload functional  components and container functions like mapStateTOprops
 (I think this is embedded into react now)
 
 
